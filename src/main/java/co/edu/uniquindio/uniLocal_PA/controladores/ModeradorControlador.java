f637fc9cefeb1d0ad4e37f62b4ddcfb380d30d3d
CamiloCortes1208 <juanc.cortesd@uqvirtual.edu.co>
2024-04-17 10:14:49 -0500

    Se han agregado las API's de revisiones en el controlador de Moderador

--- src/main/java/co/edu/uniquindio/uniLocal_PA/controladores/ModeradorControlador.java
+++ src/main/java/co/edu/uniquindio/uniLocal_PA/controladores/ModeradorControlador.java
@@ -3,14 +3,22 @@ package co.edu.uniquindio.uniLocal_PA.controladores;
 import co.edu.uniquindio.uniLocal_PA.dto.JWT_DTO.MensajeDTO;
 import co.edu.uniquindio.uniLocal_PA.dto.moderadorDTO.ActualizarModeradorDTO;
 import co.edu.uniquindio.uniLocal_PA.dto.negocioDTO.ItemNegocioDTO;
+import co.edu.uniquindio.uniLocal_PA.dto.revisionDTO.AgregarRevisionDTO;
+import co.edu.uniquindio.uniLocal_PA.dto.revisionDTO.DetalleRevisionDTO;
+import co.edu.uniquindio.uniLocal_PA.dto.revisionDTO.ItemRevisionDTO;
+import co.edu.uniquindio.uniLocal_PA.modelo.documentos.Revision;
 import co.edu.uniquindio.uniLocal_PA.modelo.enumeraciones.EstadoNegocio;
+import co.edu.uniquindio.uniLocal_PA.modelo.excepciones.ResourceNotFoundException;
 import co.edu.uniquindio.uniLocal_PA.servicios.interfaces.ClienteServicio;
 import co.edu.uniquindio.uniLocal_PA.servicios.interfaces.ModeradorServicio;
 import co.edu.uniquindio.uniLocal_PA.servicios.interfaces.NegocioServicio;
+import co.edu.uniquindio.uniLocal_PA.servicios.interfaces.RevisionServicio;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -20,24 +28,17 @@ public class ModeradorControlador {
 
     private final ModeradorServicio moderadorServicio;
     private final NegocioServicio negocioServicio;
+    private final RevisionServicio revisionServicio;
 
     @PutMapping("/editar-perfil")
     public ResponseEntity<MensajeDTO<String>>
-    actualizarModerador(ActualizarModeradorDTO actualizarModeradorDTO) throws Exception {
+    actualizarModerador(@Valid @RequestBody ActualizarModeradorDTO actualizarModeradorDTO) throws Exception {
         moderadorServicio.actualizarModerador(actualizarModeradorDTO);
         return ResponseEntity.ok().body( new MensajeDTO<>(false,
                 "Cliente actualizado correctamente"));
     }
 
     //Acciones que puede realizar un moderador respecto a los negocios
-
-    @PutMapping("/rechazar-negocio/{idNegocio}")
-    public ResponseEntity<MensajeDTO<String>>
-    rechazarNegocio(@PathVariable String idNegocio) throws Exception {
-        negocioServicio.rechazarNegocio(idNegocio);
-        return ResponseEntity.ok().body( new MensajeDTO<>(false,
-                "Negocio rechazado con éxito"));
-    }
 
     @GetMapping("/listar-negocios-estado/{estadoNegocio}")
     public ResponseEntity<MensajeDTO<List<ItemNegocioDTO>>>
@@ -46,5 +47,35 @@ public class ModeradorControlador {
                 negocioServicio.filtrarPorEstado(estadoNegocio)));
     }
 
+    //Acciones que realiza un moderador respecto a las revisiones de los negocios
+
+    @PostMapping("/agregar-revision")
+    public ResponseEntity<MensajeDTO<String>>
+    agregarRevision(@Valid @RequestBody AgregarRevisionDTO agregarRevisionDTO) throws Exception {
+        revisionServicio.agregarRevision(agregarRevisionDTO);
+        return ResponseEntity.ok().body( new MensajeDTO<>(false,
+                "Revisión registrada correctamente"));
+    }
+
+    @GetMapping("/obtener-revision/{codigoRevision}")
+    public ResponseEntity<MensajeDTO<DetalleRevisionDTO>>
+    obtenerRevision(@PathVariable String codigoRevision) throws ResourceNotFoundException {
+        return ResponseEntity.ok().body( new MensajeDTO<>(false,
+                revisionServicio.obtenerRevision(codigoRevision)));
+    }
+
+    @GetMapping("/listar-revisiones-moderador/{codigoModerador}")
+    public ResponseEntity<MensajeDTO<List<ItemRevisionDTO>>>
+    listarRevisionModerador(@PathVariable String codigoModerador) {
+        return ResponseEntity.ok().body( new MensajeDTO<>(false,
+                revisionServicio.listarRevisionModerador(codigoModerador)));
+    }
+
+    @GetMapping("/listar-revisiones-negocio/{codigoNegocio}")
+    public ResponseEntity<MensajeDTO<List<ItemRevisionDTO>>>
+    listarRevisionNegocio(@PathVariable String codigoNegocio) {
+        return ResponseEntity.ok().body( new MensajeDTO<>(false,
+                revisionServicio.listarRevisionNegocio(codigoNegocio)));
+    }
 
 }
