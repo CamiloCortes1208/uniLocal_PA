2451b8ba46c4c2a8720ff8db374f1f131167a1cd
CamiloCortes1208 <juanc.cortesd@uqvirtual.edu.co>
2024-04-17 14:58:40 -0500

    Se ha hecho una corrección a CalificacionRepo, ya que faltaba la proyección de los atributos codigoCalificacion, codigoNegocio y codigoCliente

--- src/main/java/co/edu/uniquindio/uniLocal_PA/repositorios/CalificacionRepo.java
+++ src/main/java/co/edu/uniquindio/uniLocal_PA/repositorios/CalificacionRepo.java
@@ -14,7 +14,7 @@ public interface CalificacionRepo extends MongoRepository<Calificacion, String> 
     @Aggregation({"{ $match: { codigoNegocio: ?0 } }", "{ $lookup: { from: 'negocios', localField: " +
             "'codigoNegocio', foreignField: '_id', as: 'negocio' } }", "{ $unwind: '$negocio' }", "{ " +
             "$project: { codigoCalificacion: '$codigoCalificacion', codigoNegocio: '$codigoNegocio'," +
-            " codigoCliente: '$codigoCliente', fecha: '$fecha', valoracion: '$valoracion', mensaje: '$mensaje', " +
+            " codigoCliente: '$codigoCliente', fecha: '$fecha', valoracion: '$valoracion', respuesta: '$respuesta', " +
             "nombreNegocio: '$negocio.nombre', ubicacionNegocio: '$negocio.ubicacion' } }"})
     List<ItemCalificacionDTO> listarCalificacionesNegocio(String codigoNegocio);
 
