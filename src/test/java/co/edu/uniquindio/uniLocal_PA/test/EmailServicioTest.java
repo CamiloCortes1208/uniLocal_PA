60009d68e92adc2e970d0fd05637c1ccadeab3da
CamiloCortes1208 <juanc.cortesd@uqvirtual.edu.co>
2024-04-17 11:58:17 -0500

    Se han hecho correcciones a las application.properties para el envio de correos

--- src/test/java/co/edu/uniquindio/uniLocal_PA/test/EmailServicioTest.java
+++ src/test/java/co/edu/uniquindio/uniLocal_PA/test/EmailServicioTest.java
@@ -1,4 +1,30 @@
 package co.edu.uniquindio.uniLocal_PA.test;
 
+import co.edu.uniquindio.uniLocal_PA.dto.clienteDTO.RegistroClienteDTO;
+import co.edu.uniquindio.uniLocal_PA.dto.emailDTO.EmailDTO;
+import co.edu.uniquindio.uniLocal_PA.servicios.interfaces.ClienteServicio;
+import co.edu.uniquindio.uniLocal_PA.servicios.interfaces.EmailServicio;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class EmailServicioTest {
+
+    @Autowired
+    private EmailServicio emailServicio;
+
+    @Test
+    public void enviarCorreoTest() throws Exception {
+
+        EmailDTO emailDTO = new EmailDTO(
+                "Envío de correo de prueba",
+                "Este es un correo de prueba",
+                ".com"
+        );
+
+        Assertions.assertDoesNotThrow( ()-> emailServicio.enviarCorreo(emailDTO));
+    }
+
 }
